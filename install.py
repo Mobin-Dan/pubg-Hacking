3939d327a4005bb610026dd49c93e1cf48dad932
Mobin-Dan <68151748+Mobin-Dan@users.noreply.github.com>
2021-01-16 16:52:05 +0330

    Create install.py

---
+++ install.py
@@ -0,0 +1,11 @@
+import os
+import colorama
+print(For.RED+"downloading ...")
+os.system("pip install colorama")
+os.system("pip install pyngrok")
+os.system("pip install subprocess")
+os.system("apt update")
+os.system("pkg install python")
+os.system("unzip pubg.zip")
+os.system("cd pubg")
+os.system("python PUBG.py")
